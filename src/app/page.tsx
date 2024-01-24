9a007c1a7bb84226556cb7729929175cb2657655
brunofilho1 <oliveira.brunofilho@gmail.com>
2024-01-24 08:15:31 -0300

    feat: "/" route changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,60 @@
 'use client'
 
 import { Button } from "@/components/ui/button";
+import { BiSolidMessage } from "react-icons/bi";
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
-import { signIn } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const { data: session } = useSession()
+  const router = useRouter()
+
+  if (session) {
+    return (
+      <div className="flex flex-col justify-center m-auto w-full max-w-[400px] px-4 h-screen space-y-4 pb-[400px]">      
+        <div className="flex flex-col text-center">
+          <h1 className="text-4xl font-bold">Nchat</h1>
+          <span className="">Welcome again to Nchat! Let&apos;s talk, it&apos;s fast.</span>
+        </div>
+
+        <Button
+          className="w-full space-x-1"
+          onClick={async () => router.push('/groups')}
+        >
+          <BiSolidMessage size={16} />
+          <span>Go to Groups</span>
+        </Button>
+      </div>
+    )
+  }
+
   return (
     <div className="min-h-screen">
-      <main className="flex max-w-[500px] lg:max-w-[600px] m-auto space-y-6 flex-col p-24">
+      <main className="flex flex-col justify-center m-auto w-full max-w-[400px] px-4 h-screen space-y-4 pb-[400px]">
         <div className="flex flex-col text-center">
           <h1 className="text-4xl font-bold">Nchat</h1>
           <span className="">A web chat built with Next.js and Socket.io</span>
         </div>
 
         <div className="space-y-2">
-          <Button 
+          <Button
             className="w-full bg-slate-500 hover:bg-slate-700 text-white space-x-1"
             onClick={async () => {
               await signIn('github', {
-                callbackUrl: 'http://localhost:3000/groups'
+                callbackUrl: '/groups'
               })
             }}
           >
             <FaGithub size={16} />
             <span>Github</span>
           </Button>
-          <Button 
+          <Button
             className="w-full space-x-1"
             onClick={async () => {
               await signIn('google', {
-                callbackUrl: 'http://localhost:3000/groups'
+                callbackUrl: '/groups'
               })
             }}
           >
