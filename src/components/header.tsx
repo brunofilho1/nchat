ff1352e6d62dcd6b0ae80ee1cec120ef726f196f
brunofilho1 <oliveira.brunofilho@gmail.com>
2024-01-24 15:05:15 -0300

    feat: header logo with icon

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,13 +4,17 @@ import ThemeButton from './theme-button';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/app/api/auth/[...nextauth]/route';
 import UserMenuDropdown from './user-menu-dropdown';
+import { IoMdChatboxes } from "react-icons/io";
 
 const Header = async () => {
   const session = await getServerSession(authOptions);
 
   return (
     <header className="h-12 border border-b px-4 py-1 flex items-center justify-between">
-      <span>Nchat</span>
+      <div className='flex items-center'>
+        <IoMdChatboxes />
+        <span className='font-bold'>Nchat</span>
+      </div>
       <div className='flex items-center space-x-2'>
         <ThemeButton />
         {session && <UserMenuDropdown />}     
